b62e55af9c3f879065d535aeb7f393aa9b365947
slamba <sandeep.lamba@grandparade.co.uk>
2018-09-27 23:00:20 +0200

    Removing terrafomr

--- deployment-terraform/modules/whPod/outputs.tf
+++
@@ -1,5 +0,0 @@
-### Outputs
-
-output "lb_ip" {
-  value = "${kubernetes_service.nginx_Terraform_Svc.load_balancer_ingress.0.hostname}"
-}
--- deployment-terraform/variables.tf
+++
@@ -1,5 +0,0 @@
-## Variable - Access-Key
-variable "Cluster_Name" {
-  description = "AWS-Sandbox Account Access Key"
-  default = "Poc-EKS-Cluster"
-}
